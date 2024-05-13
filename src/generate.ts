f5b2cb7ce1540ce790265712ee78b3e952df2690
Frecherenkel60 <rieger.kevin01@gmail.com>
2024-05-13 21:43:19 +0200

    ci: adjust to new structure

--- src/generate.ts
+++ src/generate.ts
@@ -1,21 +1,24 @@
 // generate.ts
-import { writeFileSync } from 'fs';
-import { fileURLToPath } from 'url';
-import {dirname, join} from 'path';
+import { writeFileSync } from "fs";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
 
 interface CustomError {
-    message: string;
-    code: string;
-    httpStatus: number;
+  title: string;
+  message: string;
+  code: string;
+  http_status: number;
 }
 
 // Calculate the directory of the current module using new ESM features
 const currentDir = dirname(fileURLToPath(import.meta.url));
 
 // Download the JSON file from the GitHub repository https://raw.githubusercontent.com/wwi21seb-projekt/error-domain/main/errors.json
-const response = await fetch('https://raw.githubusercontent.com/wwi21seb-projekt/error-domain/main/errors.json');
+const response = await fetch(
+  "https://raw.githubusercontent.com/wwi21seb-projekt/error-domain/main/errors.json"
+);
 if (!response.ok) {
-    throw new Error(`HTTP error! status: ${response.status}`);
+  throw new Error(`HTTP error! status: ${response.status}`);
 }
 
 const errors: CustomError[] = await response.json();
@@ -32,10 +35,10 @@ export class CustomError extends Error {
     }
 }\n\n`;
 
-errors.forEach(error => {
-    content += `export const ${error.code} = new CustomError(\n\t"${error.message}", \n\t"${error.code}", \n\t${error.httpStatus}\n);\n`;
+errors.forEach((error) => {
+  content += `export const ${error.title} = new CustomError(\n\t"${error.message}", \n\t"${error.code}", \n\t${error.http_status}\n);\n`;
 });
 
 // Specify the output path for the generated TypeScript file
-const outputPath = join(currentDir, '../src/errors.ts');
+const outputPath = join(currentDir, "../src/errors.ts");
 writeFileSync(outputPath, content);
